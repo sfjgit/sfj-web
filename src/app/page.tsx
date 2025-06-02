776e73814394f4eee5c2c39ca41e2d0c9c391aa4
sfjgit <ta7179@sfjbs.com>
2025-06-02 18:30:53 +0530

    almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import FootprintSection from "./_components/FootprintSection";
 import TalentServicesSection from "./_components/TalentServicesSection";
 import PartnersSection from "./_components/PartnersSection";
 import GlobalPresenceSection from "./_components/GlobalPresenceSection";
-import BlogSection from "./_components/BlogSection";
+// import BlogSection from "./_components/BlogSection";
 import CTASection from "./_components/CTASection";
 import FloatingActionButton from "./_components/FloatingActionButton";
 import SEOHead from "./_components/SeoHead";
@@ -28,7 +28,7 @@ const HomePage = () => {
       <TalentServicesSection />
       <PartnersSection />
       <GlobalPresenceSection />
-      <BlogSection />
+      {/* <BlogSection /> */}
       <CTASection />
       <FloatingActionButton />
     </div>
