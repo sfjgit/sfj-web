c31100592f9ba64fef26e30439adc67eb260524a
Binta-SFJ <dev07@sfjbs.com>
2025-10-15 14:21:31 +0530

    Added a blog

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -43,6 +43,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     "/blog/csr-it-training",
     "/blog/enterprise-training",
     "/blog/ai-is-transforming-healthcare",
+    "/blog/sap-business-technology-platform-features-benefits",
   ];
 
   // Combine all routes
